f3bc86f52c8cce0c20b917363c6aeb5b57ee1746
mushfiqurrahman-git <mymensingh456@gmail.com>
2022-10-01 12:31:40 +0600

    sec

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Listing;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        Listing::factory(6)->create();
     }
 }
